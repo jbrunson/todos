30d65998e0aaab3e3adc46c1f2225d1a63e1753b
Jeff Brunson <jeff.a.brunson@gmail.com>
2014-11-14 11:59:34 -0700

    allow todos to be completed

--- spec/features/user_sees_own_todos_spec.rb
+++ spec/features/user_sees_own_todos_spec.rb
@@ -4,7 +4,7 @@ feature "User sees own todos" do
   scenario "Doesn't see others todos" do
     Todo.create!(title: "Buy milk", email: "someone_else@example.com")
 
-    sign_in_as "someone@example.com"
+    sign_in_as "someone1@example.com"
     expect(page).not_to have_css ".todos li", text: "Buy milk"
   end
 end
